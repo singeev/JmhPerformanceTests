556c16c93d188d05c7f750895cb9e49c2fbdcea3
Alexander Singeev <singeev@loyaltyplant.com>
2016-09-15 11:47:01 +0300

    New benchmarks parameters

--- src/main/java/org/sample/BenchmarkToCompareKeyTypes.java
+++ src/main/java/org/sample/BenchmarkToCompareKeyTypes.java
@@ -23,7 +23,8 @@ public class BenchmarkToCompareKeyTypes {
 
     private static final byte[] AVERAGE_BYTE_ARRAY_KEY = ByteBuffer.allocate(12).putInt(0, 2102).putInt(4, 256987).putInt(8, 25).array();
 
-    @Param({"10000", "100000", "1000000"})
+//    @Param({"10000", "100000", "1000000"})
+    @Param({"1000000"})
     public int elementsNumber;
 
     Random random = new Random();
@@ -69,11 +70,22 @@ public class BenchmarkToCompareKeyTypes {
 
     @Benchmark
     @Fork(1)
-    public HashMap<String, String> testGet1000ElementsWithStringKey() {
+    public HashMap<String, String> testGet1000ElementsWithStringKey1() {
         HashMap<String, String> result = new HashMap<>();
         map1mStringKey.entrySet()
                 .stream()
                 .filter(e -> e.getKey().startsWith(String.valueOf(random.nextInt(1000))))
+                .forEach(e -> result.put(e.getKey(), e.getValue()));
+        return result;
+    }
+
+    @Benchmark
+    @Fork(1)
+    public HashMap<String, String> testGet1000ElementsWithStringKey2() {
+        HashMap<String, String> result = new HashMap<>();
+        map1mStringKey.entrySet()
+                .stream()
+                .filter(e -> e.getKey().contains(String.valueOf(random.nextInt(1000))))
                 .forEach(e -> result.put(e.getKey(), e.getValue()));
         return result;
     }
