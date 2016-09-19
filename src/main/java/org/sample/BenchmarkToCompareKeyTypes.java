5fbc4ea092ba648dda8d69453c63e0af61b84b6c
Alexander Singeev <singeev@loyaltyplant.com>
2016-09-19 14:54:57 +0300

    fix

--- src/main/java/org/sample/BenchmarkToCompareKeyTypes.java
+++ src/main/java/org/sample/BenchmarkToCompareKeyTypes.java
@@ -1,16 +1,11 @@
 package org.sample;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.openhft.chronicle.map.ChronicleMap;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.BenchmarkParams;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -23,7 +18,6 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class BenchmarkToCompareKeyTypes {
 
-    private static final String KEY = "keykeykeykeykeykeykeykeykeykeykeykeykeykeykey";
     private static final String VALUE = "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglongvalue";
 
     private static final byte[] AVERAGE_BYTE_ARRAY_KEY = ByteBuffer.allocate(12).putInt(0, 2102).putInt(4, 256987).putInt(8, 25).array();
@@ -34,46 +28,13 @@ public class BenchmarkToCompareKeyTypes {
 
     Random random = new Random();
 
-    private ChronicleMap<String, String> map1mStringKey;
     private ChronicleMap<String, String> map1mBigStringKey;
-    private ChronicleMap<String, String> map1mBigStringKeyJSON;
     private ChronicleMap<byte[], String> map1mByteArrayKey;
 
     @Setup
     public void setup(BenchmarkParams params) {
         System.out.println();
         System.out.println("Elements: " + elementsNumber);
-
-//        map1mStringKey = ChronicleMap
-//                .of(String.class, String.class)
-//                .averageKey("15648:keykeykeykeykeykeykeykeykeykeykeykeykeykeykey")
-//                .averageValue(VALUE)
-//                .entries(elementsNumber)
-//                .create();
-//
-//        for (int i = 0; i < elementsNumber/1000; i++) {
-//            for(int j = 0; j < 1000; j++) {//to make keys with duplicate value of a 1st parameter
-//                String key = j + ":" + KEY;
-//                String value = VALUE + i;
-//                map1mStringKey.put(key, value);
-//            }
-//        }
-//
-//        String JSON = "{\"1\":\"textForLanguageId-1\",\"2\":\"textForLanguageId-2\",\"3\":\"textForLanguageId-3\",\"4\":\"textForLanguageId-4\",\"5\":\"textForLanguageId-5\",\"6\":\"textForLanguageId-6\",\"7\":\"textForLanguageId-7\",\"8\":\"textForLanguageId-8\",\"9\":\"textForLanguageId-9\",\"10\":\"textForLanguageId-10\",\"11\":\"textForLanguageId-11\",\"12\":\"textForLanguageId-12\"}";
-//
-//        map1mBigStringKeyJSON = ChronicleMap
-//                .of(String.class, String.class)
-//                .averageKey("pid15648:cid1298754:ct34:lid1")
-//                .averageValue(JSON)
-//                .entries(elementsNumber)
-//                .create();
-//
-//        for (int i = 0; i < elementsNumber/1000; i++) {
-//            for(int j = 0; j < 1000; j++) {//to make keys with duplicate value of a 1st parameter
-//                String key = "pid" + i + ":cid" + i + ":ct" + i + 1 + ":lid" + i + 1;
-//                map1mBigStringKeyJSON.put(key, JSON);
-//            }
-//        }
 
         map1mBigStringKey = ChronicleMap
                 .of(String.class, String.class)
@@ -104,69 +65,15 @@ public class BenchmarkToCompareKeyTypes {
         System.out.println("Filled CronicleMap with " + map1mByteArrayKey.size() + " elements (byte[] key).");
     }
 
-//    @Benchmark
-    @Fork(1)
-    public HashMap<String, String> testGet1000ElementsWithStringKey1() {
-        HashMap<String, String> result = new HashMap<>();
-        map1mStringKey.entrySet()
-                .stream()
-                .filter(e -> e.getKey().startsWith(String.valueOf(random.nextInt(1000))))
-                .forEach(e -> result.put(e.getKey(), e.getValue()));
-        return result;
-    }
-
-//    @Benchmark
-    @Fork(1)
-    public HashMap<String, String> testGet1000ElementsWithStringKey2() {
-        HashMap<String, String> result = new HashMap<>();
-        map1mStringKey.entrySet()
-                .stream()
-                .filter(e -> e.getKey().contains(String.valueOf(random.nextInt(1000))))
-                .forEach(e -> result.put(e.getKey(), e.getValue()));
-        return result;
-    }
-
-//    @Benchmark
-    @Fork(1)
-    public HashMap<String, String> testGet1000ElementsWithBigStringKey() {
-        HashMap<String, String> result = new HashMap<>();
-        map1mStringKey.entrySet()
-                .stream()
-                .filter(e -> e.getKey().contains(String.valueOf("pid" + random.nextInt(1000))))
-                .forEach(e -> result.put(e.getKey(), e.getValue()));
-        return result;
-    }
-
     @Benchmark
     @Fork(1)
     public HashMap<String, String> testGet1000ElementsWithFullStringKey() {
         HashMap<String, String> result = new HashMap<>();
-        for(int i = 0; i < 1000; i++) {//to make keys with duplicate value of a 1st parameter
+        for(int i = 0; i < 1000; i++) {
             String key = "pid" + i + ":cid" + i + 1 + ":ct" + i + 2 + ":lid" + i + 3;
-            result.put(key,map1mBigStringKey.get(key));
+            result.put(key, map1mBigStringKey.get(key));
         }
         System.out.println("Got " + result.size() + " elements by String key.");
-        return result;
-    }
-
-//    @Benchmark
-    @Fork(1)
-    public HashMap<String, String> testGet1000ElementsWithFullStringKeyJSON() {
-        HashMap<String, String> result = new HashMap<>();
-        int languageId = random.nextInt(12);
-        for(int i = 0; i < 1000; i++) {
-            String key = "pid" + i + ":cid" + i + ":ct" + i + 1 + ":lid" + i + 1;
-            String jsonString = map1mBigStringKeyJSON.get(key);
-            Map<Integer, String> mapFromJson = null;
-            try {
-                mapFromJson = new ObjectMapper().readValue(
-                        jsonString, new TypeReference<HashMap<Integer, String>>() {
-                        });
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            result.put(key, mapFromJson.get(languageId));
-        }
         return result;
     }
 
@@ -174,9 +81,9 @@ public class BenchmarkToCompareKeyTypes {
     @Fork(1)
     public HashMap<byte[], String> testGet1000ElementsWithByteArrayKey() {
         HashMap<byte[], String> result = new HashMap<>();
-        for(int i = 0; i < 1000; i++) {//to make keys with duplicate value of a 1st parameter
+        for(int i = 0; i < 1000; i++) {
             byte[] key = ByteBuffer.allocate(12).putInt(0, i).putInt(4, i + 1).putInt(8, i + 2).array();
-            result.put(key, map1mBigStringKey.get(key));
+            result.put(key, map1mByteArrayKey.get(key));
         }
         System.out.println("Got " + result.size() + " elements by byte[] key.");
         return result;
